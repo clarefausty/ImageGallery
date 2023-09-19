c2cc7f71a9f708943feee6300dc892f3bf0a21a1
clarefausty <clarefausty@gmail.com>
2023-09-20 00:20:20 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
 import {  useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import "./App.css";
+import DATA from "./Component/DATA";
 
-const DATA = [
-  {
-    id: "0e2f0db1-5457-46b0-949e-8032d2f9997a",
-    name: "/Asset/1a.jpg",
-    
-  },
-  {
-    id: "487f68b4-1746-438c-920e-d67b7df46247",
-    name:  "/Asset/1b.jpg",
-    
-  },
-  {
-    id: "25daffdc-aae0-4d73-bd31-43f73101e7c0",
-    name:  "/Asset/1c.jpg",
-    
-  },
 
-  {
-    id: "d3edf796-6449-4931-a777-ff66965a025b",
-    name:  "/Asset/1d.jpg",
-  },
 
-  {
-    id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc",
-    name:  "/Asset/1e.jpg",
-  },
-  {
-    id: "5bee94eb-6bde-4411-b438-1c37fa6af364",
-    name:  "/Asset/1f.jpg",
-  },
-  {
-    id: "95ee6a5d-f927-4579-8c15-2b4eb86210ae",
-    name:  "/Asset/1i.jpg",
-  },
-  {
-    id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525",
-    name:  "/Asset/1j.jpg",
-  }
-
-];
 
 
 function App() {
--- src/Component/data.js
+++ src/Component/data.js
@@ -1,41 +1,41 @@
-const data =
-[
-   {
-       id: 1,
-       imgURL: "/Asset/1a.jpg"
-       
-   },
-
-   {
-    id: 2,
-    imgURL: "/Asset/1b.jpg"
-},
+const DATA = [
     {
-       id: 3,
-       imgURL: "/Asset/1c.jpg"
-   },
-   {
-       id: 4,
-       imgURL: "/Asset/1d.jpg"
-   },
-
-   {
-    id: 5 ,
-    imgURL: "/Asset/1e.jpg"
-    },
-
-    {
-        id: 6,
-        imgURL: "/Asset/1f.jpg"
+      id: "0e2f0db1-5457-46b0-949e-8032d2f9997a",
+      name: "/Asset/1a.jpg",
+      
     },
     {
-        id: 7,
-        imgURL: "/Asset/1g.jpg"
+      id: "487f68b4-1746-438c-920e-d67b7df46247",
+      name:  "/Asset/1b.jpg",
+      
     },
     {
-        id: 8,
-        imgURL: "/Asset/1h.jpg"
+      id: "25daffdc-aae0-4d73-bd31-43f73101e7c0",
+      name:  "/Asset/1c.jpg",
+      
+    },
+  
+    {
+      id: "d3edf796-6449-4931-a777-ff66965a025b",
+      name:  "/Asset/1d.jpg",
+    },
+  
+    {
+      id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc",
+      name:  "/Asset/1e.jpg",
+    },
+    {
+      id: "5bee94eb-6bde-4411-b438-1c37fa6af364",
+      name:  "/Asset/1f.jpg",
+    },
+    {
+      id: "95ee6a5d-f927-4579-8c15-2b4eb86210ae",
+      name:  "/Asset/1i.jpg",
+    },
+    {
+      id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525",
+      name:  "/Asset/1j.jpg",
     }
-
-]
-export default data;
+  
+  ];
+export default DATA;
