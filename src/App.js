6443e2c6652ff08c1c01d374cdfb8a2cd407d36e
clarefausty <clarefausty@gmail.com>
2023-09-19 16:30:31 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,45 +1,152 @@
+
+
+// import React, { useState } from 'react';
+// import data from './Component/data';
+// import './App.css';
+// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+// function App() {
+//   const [images, setImages] = useState(data);
+
+//   const onDragEnd = (result) => {
+//     if (!result.destination) return;
+
+//     const newImages = [...images];
+//     const [reorderedImage] = newImages.splice(result.source.index, 1);
+//     newImages.splice(result.destination.index, 0, reorderedImage);
+
+//     setImages(newImages);
+//   };
+
+//   return (
+//     <div className='main'>
+//       <div>
+//       <DragDropContext onDragEnd={onDragEnd}>
+//         <Droppable droppableId='gallery' type='group'>
+//           {(provided) => (
+//             <div
+//               className='gallery'
+//               {...provided.droppableProps}
+//               ref={provided.innerRef}
+//             >
+//               {images.map((segment, index) => (
+//                 <Draggable
+//                   key={segment.id}
+//                   draggableId={segment.id.toString()}
+//                   index={index}
+//                 >
+//                   {(provided) => (
+//                     <div
+//                       ref={provided.innerRef}
+//                       {...provided.draggableProps}
+//                       {...provided.dragHandleProps}
+//                     >
+//                       <img src={segment.imgURL} alt='Nice Perfumes' />
+//                     </div>
+//                   )}
+//                 </Draggable>
+//               ))}
+//               {provided.placeholder}
+//             </div>
+//           )}
+//         </Droppable>
+//       </DragDropContext>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React, { useState } from 'react';
-import ImageSetup from './Component/ImageSetup/ImageSetup';
 import data from './Component/data';
+import './App.css';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
-  const [images, setImages] = useState(data);
+  const [stores, setStores] = useState(data);
+  const handleDragAndDrop = (results) => {
+    const { source, destination, type } = results;
 
-  // Function to handle the drag-and-drop reordering
-  const onDragEnd = (result) => {
-    if (!result.destination) return;
+    if (!destination) return;
 
-    const items = [...images];
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    )
+      return;
 
-    setImages(items);
+    if (type === "group") {
+      const reorderedStores = [...stores];
+
+      const storeSourceIndex = source.index;
+      const storeDestinatonIndex = destination.index;
+
+      const [removedStore] = reorderedStores.splice(storeSourceIndex, 1);
+      reorderedStores.splice(storeDestinatonIndex, 0, removedStore);
+
+      return setStores(reorderedStores);
+    }
+    const itemSourceIndex = source.index;
+    const itemDestinationIndex = destination.index;
+
+    const storeSourceIndex = stores.findIndex(
+      (store) => store.id === source.droppableId
+    );
+    const storeDestinationIndex = stores.findIndex(
+      (store) => store.id === destination.droppableId
+    );
+
+    const newSourceItems = [...stores[storeSourceIndex].items];
+    const newDestinationItems =
+      source.droppableId !== destination.droppableId
+        ? [...stores[storeDestinationIndex].items]
+        : newSourceItems;
+
+    const [deletedItem] = newSourceItems.splice(itemSourceIndex, 1);
+    newDestinationItems.splice(itemDestinationIndex, 0, deletedItem);
+
+    const newStores = [...stores];
+
+    newStores[storeSourceIndex] = {
+      ...stores[storeSourceIndex],
+      items: newSourceItems,
+    };
+    newStores[storeDestinationIndex] = {
+      ...stores[storeDestinationIndex],
+      items: newDestinationItems,
+    };
+
+    setStores(newStores);
   };
 
+  
   return (
     <div className='main'>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId='gallery'>
+      <div>
+      <DragDropContext onDragEnd={handleDragAndDrop}>
+        <Droppable droppableId='gallery' type='group'>
           {(provided) => (
             <div
               className='gallery'
               {...provided.droppableProps}
               ref={provided.innerRef}
             >
-              {images.map((segment, index) => (
+              {stores.map((segment, index) => (
                 <Draggable
                   key={segment.id}
-                  draggableId={`image-${segment.id}`}
+                  draggableId={segment.id.toString()}
                   index={index}
                 >
                   {(provided) => (
-                    
-                    <ImageSetup imgURL={segment.imgURL}
-                    ref={provided.innerRef}
+                    <div className='img-div'
+                      ref={provided.innerRef}
                       {...provided.draggableProps}
                       {...provided.dragHandleProps}
-                    />      
+                    >
+                      <img src={segment.imgURL} alt='Nice Perfumes' />
+                    </div>
                   )}
                 </Draggable>
               ))}
@@ -48,6 +155,7 @@ function App() {
           )}
         </Droppable>
       </DragDropContext>
+      </div>
     </div>
   );
 }
--- src/Component/data.js
+++ src/Component/data.js
@@ -3,6 +3,7 @@ const data =
    {
        id: 1,
        imgURL: "/Asset/1a.jpg"
+       
    },
 
    {
@@ -26,6 +27,14 @@ const data =
     {
         id: 6,
         imgURL: "/Asset/1f.jpg"
+    },
+    {
+        id: 7,
+        imgURL: "/Asset/1g.jpg"
+    },
+    {
+        id: 8,
+        imgURL: "/Asset/1h.jpg"
     }
 
 ]
