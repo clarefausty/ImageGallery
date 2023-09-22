e817ed21a9437322d402bbbba78ed195be16d0c0
clarefausty <clarefausty@gmail.com>
2023-09-22 11:05:44 +0100

    wip

--- src/Component/auth/RegisterAndLogin.js
+++ src/Component/auth/RegisterAndLogin.js
@@ -1,54 +1,45 @@
-import React, {useState} from "react"
+import React from "react"
 import { database } from "../../firebase"
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword} from "firebase/auth"
 import { useNavigate } from "react-router-dom"
 import "./RegisterAndLogin.css"
 
 function RegisterAndLogin(){
-    const[login, setLogin] = useState(false)
 
     const history = useNavigate()
 
-    const handleSubmit =(e, type)=>{
+    const handleSubmit =(e)=>{
         e.preventDefault()
         const email = e.target.email.value
         const password = e.target.password.value
-        if(type === 'signup'){
+       
         createUserWithEmailAndPassword(database, email, password).then(data=>{
             console.log(data, "authData")
             history("/home")
 
         }).catch(err=>{
             alert(err.code)
-            setLogin(true)
+           
         })
-    }else{
-        signInWithEmailAndPassword(database, email, password).then(data=>{
-            console.log(data, "authData")
-            history("/home")
-
-        }).catch(err=>{
-            alert(err.code)
-        })
-    }
+    
     }
     return(
         <div className="login-container">
-            <div className="log-status">
-        <div className={login === false? 'activeColor':'pointer'} onClick={(()=>setLogin(false))}>Sign Up</div>
-        <div className={login === true? 'activeColor':'pointer'} onClick={(()=>setLogin(true))}>Sign In</div>
-            </div>
-            <h1>{login? 'Signin' : 'Signup'}</h1>
-            <form onSubmit={(e)=>handleSubmit(e, login? 'Signin' : 'Singnup')}>
+            
+            <h1>Signin</h1>
+            <form onSubmit={(e)=>handleSubmit(e)}>
                 <input type="email" name="email" placeholder="Email"/>
                 <input type="password" name="password" placeholder="password"/>
                 <div className="btn-container">
-                <button>{login? 'Signin':'Signup'}</button>
+                <button>Signin</button>
                 </div>
             </form>
         </div>
     )
 }
 export default RegisterAndLogin
+
+
+
 
 
